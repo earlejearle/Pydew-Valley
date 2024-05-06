777e572b8873c74a3e607382c0691848be47e3f0
Jai Earle <jaiearley@hotmail.com>
2024-05-06 17:40:34 +1000

    Apples on trees, and chopping down trees

--- code/sprites.py
+++ code/sprites.py
@@ -1,5 +1,7 @@
 import pygame
 from settings import *
+from random import randint, choice
+from timer import Timer
 
 
 class Generic(pygame.sprite.Sprite):
@@ -26,7 +28,7 @@ class Water(Generic):
         )
 
     def animate(self, dt):
-        self.frame_index += 5 * dt
+        self.frame_index += 4 * dt
         if self.frame_index > len(self.frames):
             self.frame_index = 0
         self.image = self.frames[int(self.frame_index)]
@@ -44,3 +46,47 @@ class WildFlower(Generic):
 class Tree(Generic):
     def __init__(self, pos, surf, groups, name):
         super().__init__(pos, surf, groups)
+
+        # tree attributes
+        self.health = 5
+        self.alive = True
+        stump_path = f'../graphics/stumps/{"small" if name == "Small" else "large"}.png'
+        self.stump_surf = pygame.image.load(stump_path).convert_alpha()
+        self.invul_timer = Timer(200)
+
+        # apples
+        self.apple_surf = pygame.image.load('../graphics/fruit/apple.png').convert_alpha()
+        self.apple_pos = APPLE_POS[name]
+        self.apple_sprites = pygame.sprite.Group()
+        self.create_fruit()
+
+    def damage(self):
+        self.health -= 1
+
+        # remove an apple
+        if len(self.apple_sprites) > 0:
+            random_apple = choice(self.apple_sprites.sprites())
+            random_apple.kill()
+
+    def check_death(self):
+        if self.health <= 0:
+            self.image = self.stump_surf
+            self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
+            self.hitbox = self.rect.copy().inflate(-10, -self.rect.height * 0.6)
+            self.alive = False
+
+    def create_fruit(self):
+        for pos in self.apple_pos:
+            if randint(0, 10) < 2:
+                x = self.rect.left + pos[0]
+                y = self.rect.top + pos[1]
+                Generic(
+                    pos=(x, y),
+                    surf=self.apple_surf,
+                    groups=[self.apple_sprites, self.groups()[0]],
+                    z=LAYERS['fruit']
+                )
+
+    def update(self, dt):
+        if self.alive:
+            self.check_death()
